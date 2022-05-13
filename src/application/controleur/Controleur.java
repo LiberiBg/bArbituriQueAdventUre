c712ac2aaa2cdc076f568ec5a7d65c29ab3743db
nathanfnl <101125369+nathanfnl@users.noreply.github.com>
2022-05-13 14:52:31 +0200

    Add files via upload

---
+++ src/application/controleur/Controleur.java
@@ -0,0 +1,45 @@
+package application.controleur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.modele.Terrain;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+public class Controleur implements Initializable{
+	
+	@FXML
+	private TilePane terrainMap;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		terrainMap.setMaxSize(20*16, 10*16);
+		this.initCarte();
+	}
+	
+	public void initCarte() {
+		ImageView img = null;
+		Terrain ter = new Terrain();
+		int[] terrain = ter.getTerrain();
+		for (int i=0; i < terrain.length; i++) {
+			switch (terrain[i]) {
+			case -1:
+				img = new ImageView(new Image("application/ressource/tile-1.png"));
+				break;
+			case 20:
+				img = new ImageView(new Image("application/ressource/tile20.png"));
+				break;
+			case 21:
+				img = new ImageView(new Image("application/ressource/tile21.png"));
+				break;
+			}
+			terrainMap.getChildren().add(img);
+		}
+		
+	}
+
+}
