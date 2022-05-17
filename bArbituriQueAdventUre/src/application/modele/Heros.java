659f4cd30f354c70a894c24527ab26fab93a0514
Kappa <30351181+LiberiBg@users.noreply.github.com>
2022-05-17 16:15:43 +0200

    Add files via upload

--- bArbituriQueAdventUre/src/application/modele/Heros.java
+++ bArbituriQueAdventUre/src/application/modele/Heros.java
@@ -4,37 +4,17 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Heros extends Personnage{
-
-	private IntegerProperty x, y ;
 	
 	public Heros(int x, int y) {
 		super();
-		this.x= new SimpleIntegerProperty(x);
-		this.y = new SimpleIntegerProperty(y);
-	}
-	
-	public IntegerProperty getX() {
-		return x;
-	}
-	
-	public IntegerProperty getY() {
-		return y ;
-	}	
-
-	public void setX(int n){
-		x.setValue(n);
-	}
-	
-	public void setY(int n){
-		y.setValue(n);
 	}
 	
 	public void avancer () {
-		this.x.setValue(this.x.getValue() + 8);
+		setX(this.x.getValue() + 8);
 	}
 	
 	public void reculer() {
-		this.x.setValue(this.x.getValue() - 8);
+		setX(XProperty() - 8);
 	}
 	
 	public void sauter() {
--- bArbituriQueAdventUre/src/application/modele/Personnage.java
+++ bArbituriQueAdventUre/src/application/modele/Personnage.java
@@ -1,10 +1,36 @@
 package application.modele;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public abstract class Personnage {
 
 		private IntegerProperty x, y;
 		private int vie;
 		private int vitesse;
+		
+		public Personnage() {
+			this.x= new SimpleIntegerProperty();
+			this.y = new SimpleIntegerProperty();
+		}
+		public IntegerProperty XProperty() {
+			return x;
+		}
+		
+		public IntegerProperty YProperty() {
+			return y ;
+		}	
+
+		public void setX(int n){
+			x.setValue(n);
+		}
+		
+		public void setY(int n){
+			y.setValue(n);
+		}
+		
+//		public boolean checkCaseEnDessous() {
+//			IntegerProperty yDuPersonnage = this.y;
+//			
+//		}
 }
