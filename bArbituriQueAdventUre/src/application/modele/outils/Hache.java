7de92033a863e524d5883c73542ed658f6c1363e
nathanfnl <101125369+nathanfnl@users.noreply.github.com>
2022-05-31 11:31:39 +0200

    Delete bArbituriQueAdventUre directory

--- bArbituriQueAdventUre/src/application/modele/outils/Hache.java
+++
@@ -1,9 +0,0 @@
-package application.modele.outils;
-
-public class Hache extends Outils{
-
-	public Hache(String cheminSprite, int quantité, int quantitéMax) {
-		super(cheminSprite, quantité, quantitéMax);
-	}
-
-}
