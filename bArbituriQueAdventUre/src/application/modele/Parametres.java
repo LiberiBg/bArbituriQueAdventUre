5ef55b08b23c4deea110f7a6402c369cc3489d69
mathe <mathe@Da-pc>
2022-06-09 20:31:38 +0100

    J'ai ajouter la range de sorte a ce que le héro ne puisse pas détruire un bloc si celui-ci est trop éloigné

--- bArbituriQueAdventUre/src/application/modele/Parametres.java
+++ bArbituriQueAdventUre/src/application/modele/Parametres.java
@@ -3,8 +3,18 @@ package application.modele;
 public class Parametres {
 
 	private final static String cheminTerrain = "application/ressource/LE DIEZ EST LA";
+	private final static int nbrColonnes = 30;
+	private final static int nbrLignes = 17;
 	
 	public static String getCheminTerrain() {
 		return cheminTerrain;
+	}
+
+	public static int getNbrcolonnes() {
+		return nbrColonnes;
+	}
+
+	public static int getNbrlignes() {
+		return nbrLignes;
 	}
 }
